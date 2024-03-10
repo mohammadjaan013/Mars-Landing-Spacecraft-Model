a6b78c4e64bea6729af936d9a357780a8d6de8a8
MULANI MOHAMMAD JAAN ABDUL RAUF <143719404+mohammadjaan013@users.noreply.github.com>
2024-03-10 20:36:10 +0530

    Add files via upload

---
+++ Arduino_codes/ultra_led/sketch_mar11c.ino
@@ -0,0 +1,60 @@
+const int trig = 12;
+const int echo = 13;
+
+const int LED1 = 8;
+const int LED2 = 7;
+const int LED3 = 6;
+
+int duration = 0;
+int distance = 0;
+
+void setup() 
+{
+  pinMode(trig , OUTPUT);
+  pinMode(echo , INPUT);
+  
+  pinMode(LED1 , OUTPUT);
+  pinMode(LED2 , OUTPUT);
+  pinMode(LED3 , OUTPUT);
+  
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  digitalWrite(trig , HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trig , LOW);
+
+
+  duration = pulseIn(echo , HIGH);
+  distance = (duration/2) / 28.5 ;
+  Serial.println(distance);
+  
+
+  if ( distance <= 7 )
+  {
+    digitalWrite(LED1, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED1, LOW);
+  }
+  if ( distance <= 14 )
+  {
+    digitalWrite(LED2, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED2, LOW);
+  }
+  if ( distance <= 21 )
+  {
+    digitalWrite(LED3, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED3, LOW);
+  }
+}
